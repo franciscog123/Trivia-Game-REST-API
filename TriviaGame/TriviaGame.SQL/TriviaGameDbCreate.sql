ecda79a9ae4e4a1028c2fb84425b49e19134bdff
Logan Westbrook <lmw170330@utdallas.edu>
2019-05-20 14:47:47 -0500

    added .sql file that creates db

---
+++ TriviaGame/TriviaGame.SQL/TriviaGameDbCreate.sql
@@ -0,0 +1,67 @@
+create table [User] (
+	UserId int identity,
+	UserName nvarchar(20) not null,
+	Email nvarchar(100) not null,
+	CompletedQuizzes int,
+	constraint PK_UserId primary key(UserId)
+)
+
+create table Quiz (
+	QuizId int identity,
+	UserId int not null,
+	CatagoryId int not null,
+	GameModeId int not null,
+	Score int not null,
+	Time datetime not null,
+	constraint PK_QuizId primary key(QuizId)
+)
+
+create table Catagory (
+	CatagoryId int identity,
+	Catagory nvarchar(20),
+	constraint PK_CatagoryId primary key(CatagoryId)
+)
+
+create table GameMode (
+	GameModeId int identity,
+	GameMode nvarchar(100),
+	constraint PK_GameModeId primary key(GameModeId)
+)
+
+create table Question (
+	QuestionId int identity,
+	CatagoryId int not null,
+	Question nvarchar(200) not null,
+	Value int not null,
+	constraint PK_QuestionId primary key(QuestionId)
+)
+
+create table QuizQuestion (
+	QuizQuestionId int identity,
+	QuizId int not null,
+	QuestionId int not null,
+	constraint PK_QuizQuestionId primary key(QuizQuestionId)
+)	
+
+create table Choice (
+	ChoiceId int identity,
+	QuestionId int not null,
+	Correct bit,
+	Choice nvarchar(100),
+	constraint PK_ChoiceId primary key(ChoiceId)
+)
+
+alter table Quiz add
+	constraint FK_UserId foreign key(UserId) references [User](UserId),
+	constraint FK_CatagoryId1 foreign key(CatagoryId) references Catagory(CatagoryId),
+	constraint FK_GameModeId foreign key(GameModeId) references GameMode(GameModeId)
+
+alter table QuizQuestion add
+	constraint FK_QuizId foreign key(QuizId) references Quiz(QuizId),
+	constraint FK_QuestionId1 foreign key(QuestionId) references Question(QuestionId)
+
+alter table Choice add
+	constraint FK_QuestionId2 foreign key(QuestionId) references Question(QuestionId)
+
+alter table Question add
+	constraint FK_CatagoryId2 foreign key(CatagoryId) references Catagory(CatagoryId)
