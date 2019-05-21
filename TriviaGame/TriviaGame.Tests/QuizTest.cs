d93e0ab8179bc5d5b18b98dfa5d0c06a0371d0e2
Francisco Guerrero <guerrerof615@gmail.com>
2019-05-21 16:06:52 -0500

    fixed tests

--- TriviaGame/TriviaGame.Tests/QuizTest.cs
+++ TriviaGame/TriviaGame.Tests/QuizTest.cs
@@ -23,8 +23,8 @@ namespace TriviaGame.Tests
             // "Empty" would throw an exception if it received a null value.
             // that would result in a failed test as expected, but this way is
             // a bit cleaner.
-            Assert.NotNull(quiz.QuizQuestions);
-            Assert.Empty(quiz.QuizQuestions);
+            Assert.NotNull(quiz.Questions);
+            Assert.Empty(quiz.Questions);
         }
     }
 }
