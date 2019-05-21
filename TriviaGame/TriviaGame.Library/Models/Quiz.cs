7d344650075fbfc4eef8aa763178d770e38a1bff
Francisco Guerrero <guerrerof615@gmail.com>
2019-05-21 10:04:01 -0500

    setting up quiz tests

--- TriviaGame/TriviaGame.Library/Models/Quiz.cs
+++ TriviaGame/TriviaGame.Library/Models/Quiz.cs
@@ -6,11 +6,23 @@ namespace TriviaGame.Library.Models
 {
     public class Quiz
     {
+        private int _score;
         public int QuizId { get; set; }
         public int UserId { get; set; }
         public int CategoryId { get; set; }
         public int GameModeId { get; set; }
-        public int Score { get; set; }
+        public int Score
+        {
+            get => _score;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Score cannot be less than 0", nameof(value));
+                }
+                _score = value;
+            }
+        }
         public DateTime Time { get; set; }
         public List<QuizQuestion> QuizQuestions { get; set; }
         
---
+++ TriviaGame/TriviaGame.Tests/QuizTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TriviaGame.Library.Models;
+using Xunit;
+
+namespace TriviaGame.Tests
+{
+    public class QuizTest
+    {
+        readonly Quiz quiz = new Quiz();  
+        
+        [Fact]
+        public void Negative_ScoreThrowsException()
+        {
+
+        }
+    }
+}
