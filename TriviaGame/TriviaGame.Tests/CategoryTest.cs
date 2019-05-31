faa4eb9134c4cea46e950066729544642afb22fe
Rodrigo Salomon <rodsalomon10@hotmail.com>
2019-05-31 14:34:19 -0500

    first tests

--- TriviaGame/TriviaGame.Tests/CategoryTest.cs
+++ TriviaGame/TriviaGame.Tests/CategoryTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using TriviaGame.Library.Models;
 using Xunit;
+using TriviaGame.DataAccess.Repositories;
+using TriviaGame.Library.Interfaces;
 
 namespace TriviaGame.Tests
 {
@@ -15,5 +17,41 @@ namespace TriviaGame.Tests
         {
             Assert.ThrowsAny<ArgumentException>(() => category.CategoryString = string.Empty);
         }
+        [Fact]
+        public void CategoryId_get()
+        {
+            Category category = new Category();
+            category.CategoryId = 1;
+            Assert.Equal(1, category.CategoryId);
+        }
+        [Fact]
+        public void CategoryString_get()
+        {
+            Category category = new Category();
+            category.CategoryString = "History";
+            Assert.Equal("History", category.CategoryString);
+        }
+
+        //public ICategoryRepository CategoryRepo { get; set; }
+
+        //[Fact]
+        //public void CategoryRep_getCategories()
+        //{
+        //    //System.Diagnostics.Debugger.Launch();
+        //    IEnumerable<Category> categories = (IEnumerable<Category>)CategoryRepo.GetCategories();
+        //    //Assert.Equal("History", category.CategoryString);
+        //    //Category category = categories[0];
+        //    //Assert.Equal("Movies", category.CategoryString);
+        //    int x = 0;
+        //    foreach (var category in categories)
+        //    {
+        //       // Assert.Equal("Movies", category.CategoryString);
+        //        x++;
+        //    }
+        //    Assert.Equal(0, x);
+        //}
+
+
+
     }
 }
--- TriviaGame/TriviaGame.Tests/UserTest.cs
+++ TriviaGame/TriviaGame.Tests/UserTest.cs
@@ -22,5 +22,16 @@ namespace TriviaGame.Tests
 
             Assert.Throws<ArgumentException>(() => user.UserName = name);
         }
+        [Fact]
+        public void User_GetSet()
+        {
+            var user = new User();
+            var name = "hotrod69";
+            var id = 5;
+            user.UserName = name;
+            user.UserId = id;
+            Assert.True(user.UserName == "hotrod69");
+            Assert.True(user.UserId == 5);
+        }
     }
 }
